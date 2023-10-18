685d989b429cbff26b3406f18138b41452404e85
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-10-18 22:42:24 +0300

    assignment complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+        sorted_word = sorted(self.word.lower())  # Sort the characters of the input word for comparison
+        for candidate in word_list:
+            # Convert the candidate to lowercase and compare sorted characters
+            if candidate.lower() != self.word.lower() and sorted(candidate.lower()) == sorted_word:
+                anagrams.append(candidate)
+        return anagrams
